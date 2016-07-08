22e2f88f6a59305c52b84ffc34c5d70303008f1c
jasells <joshua.sells@gmail.com>
2016-07-08 12:55:15 -0500

    UWP builds and runs, but no sound

---
+++ XamarinAudioTest/XamarinAudioTest.UWP/WinPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Xamarin.Forms.Dependency(typeof(XamarinAudioTest.UWP.WinPlayer))]
+
+
+namespace XamarinAudioTest.UWP
+{
+    public class WinPlayer : IAudioPlayer
+    {
+        public void PlayStream(Uri link)
+        {
+            App.Player.Source = link;
+            App.Player.Play();
+        }
+    }
+}
---
+++ XamarinAudioTest/XamarinAudioTest/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinAudioTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DependencyService.Get<IAudioPlayer>()
+                .PlayStream(new Uri("http://www.westhuntsville.org/Audio/Uploaded/TestFile.mp3"));
+        }
+    }
+}
